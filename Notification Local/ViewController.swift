1f9ad7e86a938280b709da1cb4d6d7b9753e8d42
Naveen <navindogiparthi@gmail.com>
2019-07-17 17:53:37 +0530

    User Local Notification

--- Notification Local/ViewController.swift
+++ Notification Local/ViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Step 1: Ask for permission
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.carPlay, .sound]) { (granted, error) in
+        }
+        
+        // Step 2: Create the notification content
+        let content = UNMutableNotificationContent()
+        content.title = "Hai I'm a notification!"
+        content.body = "Look at me!"
+        
+        // Step 3: Create the notification trigger
+        let date = Date().addingTimeInterval(15)
+        
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        
+        // Step 4: Create the request
+        
+        let uuidString = UUID().uuidString
+        
+        let request = UNNotificationRequest(identifier: uuidString, content: content, trigger: trigger)
+        print(uuidString)
+        // Step 5: Register the request
+        center.add(request) { (error) in
+            // Check the error parameter and handle any errors
+        }
     }
 
 
